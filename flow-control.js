264d94e5ee856b1489f24501c079f0a93fe2b11e
Paige Burlew <paigeburlew@gmail.com>
2020-06-19 21:40:31 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -24,8 +24,9 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-return age <=13 && age >=19 ? "You are a teenager" :
-"You are not a teenager"
+if (age >=13 && age <=19) {
+  return "You are a teenager" ;
+} else { return "You are not a teenager"
 }
 
 function switchAge(age) {
